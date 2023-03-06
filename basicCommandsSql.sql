107f683f508ecbcf2948405d445e214f72f25a77
Nerwinn <123543405+Nerwinn@users.noreply.github.com>
2023-03-06 18:15:56 +0800

    Add files via upload

---
+++ basicCommandsSql.sql
@@ -0,0 +1,32 @@
+-- ALL TABLES in STUDENTS database
+SELECT * FROM compsci;
+SELECT * FROM city;
+SELECT * FROM faculty;
+
+-- Updating a table
+UPDATE faculty SET salary = 21500 WHERE id_no = 4;
+
+-- Adding column in a table
+ALTER TABLE faculty
+ADD COLUMN salary BIGINT;
+
+--Basic Subquery
+SELECT 
+	firstname,
+	lastname,
+	age
+FROM 
+	compsci
+WHERE
+	age < (SELECT AVG(age) FROM compsci);
+
+	
+-- Basic WINDOW function
+SELECT *,
+	AVG(salary) OVER(PARTITION BY position) average_salary,
+	RANK() OVER(PARTITION BY position ORDER BY salary) rnk,
+	ROW_NUMBER() OVER(PARTITION BY position ORDER BY salary) rn,
+	DENSE_RANK() OVER(PARTITION BY position ORDER BY salary) dr 
+FROM faculty
+
+
